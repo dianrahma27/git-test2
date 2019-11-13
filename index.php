15ef24fa6df6577bd9b8d9788e04d60d9a50fff7
Dian Rahmawati <dianrahmawati327@gmail.com>
2019-11-13 11:15:36 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h2>Ayo Semangat Belajar</h2>
+</body>
+</html>
