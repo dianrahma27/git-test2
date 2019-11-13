83f9b52da09aa7e7f13b32077b97dc022e6e0e1f
Dian Rahmawati <dianrahmawati327@gmail.com>
2019-11-13 11:23:27 +0700

    Menambahkan daftar belajar di index.php

--- index.php
+++ index.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Coba Git Remote</title>
+    <title>Coba Git Remote</title>    
 </head>
 <body>
-    <h2>Ayo Semangat Belajar</h2>
+    <h2>Ayo Semangat Belajar Yan :)</h2>
+    <ul>
+        <li> Redux</li>
+        <li> React JS & life Cycle</li>
+        <li> Material Design</li>
+    </ul>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quo a mollitia facilis, rerum deserunt, quidem laudantium libero facere illum corrupti. Facilis ducimus provident, facere velit vero dicta quae ullam.</p>
 </body>
 </html>
